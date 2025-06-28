fd265253cd9ebf1ad3bf19ca1185384dd35115d2
Nao Suzuki <nao.suzuki@gmail.com>
2025-06-28 15:39:33 -0400

    update

--- programs_plot/00_concat_desidr1_csv.py
+++ programs_plot/00_concat_desidr1_csv.py
@@ -1,17 +1,57 @@
 import pandas as pd
 
+# DESI DR1
 csvfile1='../csvfiles/gaiadr3_desidr1_star1.csv'
 csvfile2='../csvfiles/gaiadr3_desidr1_star2.csv'
+
+# DESI DR2
+csvfile1='../csvfiles/gaiadr3_desidr2_star1.csv'
+csvfile2='../csvfiles/gaiadr3_desidr2_star2.csv'
+csvfile3='../csvfiles/gaiadr3_desidr2_star3.csv'
+csvfile4='../csvfiles/gaiadr3_desidr2_star4.csv'
+csvfile5='../csvfiles/gaiadr3_desidr2_star5.csv'
+
+def concat_desi_dr2(csvfile1,csvfile2,csvfile3,csvfile4,csvfile5):
+   dftmp1=pd.read_csv(csvfile1)
+   df1=dftmp1[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
+            'non_single_star','phot_variable_flag','ruwe',\
+            'phot_variable_flag']]
+
+   dftmp2=pd.read_csv(csvfile2)
+   df2=dftmp2[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
+            'non_single_star','phot_variable_flag','ruwe',\
+            'phot_variable_flag']]
+
+   dftmp3=pd.read_csv(csvfile3)
+   df3=dftmp3[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
+            'non_single_star','phot_variable_flag','ruwe',\
+            'phot_variable_flag']]
+
+   dftmp4=pd.read_csv(csvfile4)
+   df4=dftmp4[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
+            'non_single_star','phot_variable_flag','ruwe',\
+            'phot_variable_flag']]
+
+   dftmp5=pd.read_csv(csvfile5)
+   df5=dftmp5[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
+            'non_single_star','phot_variable_flag','ruwe',\
+            'phot_variable_flag']]
+
+   df6=pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
+   df6.to_csv('../csvfiles/gaiadr3_desidr2_star.csv')
    
-dftmp1=pd.read_csv(csvfile1)
-df1=dftmp1[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
+def concat_desi_dr1(csvfile1,csvfile2):
+   dftmp1=pd.read_csv(csvfile1)
+   df1=dftmp1[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
             'non_single_star','phot_variable_flag','ruwe',\
             'phot_variable_flag']]
 
-dftmp2=pd.read_csv(csvfile2)
-df2=dftmp2[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
+   dftmp2=pd.read_csv(csvfile2)
+   df2=dftmp2[['phot_g_mean_mag','bp_rp','parallax','parallax_over_error',\
             'non_single_star','phot_variable_flag','ruwe',\
             'phot_variable_flag']]
 
-df3=pd.concat([df1, df2], ignore_index=True)
-df3.to_csv('../csvfiles/gaiadr3_desidr1_star.csv')
+   df3=pd.concat([df1, df2], ignore_index=True)
+   df3.to_csv('../csvfiles/gaiadr3_desidr1_star.csv')
+
+concat_desi_dr2(csvfile1,csvfile2,csvfile3,csvfile4,csvfile5)
--- programs_plot/01_plot_HRdiagram_SNR.py
+++ programs_plot/01_plot_HRdiagram_SNR.py
@@ -221,10 +221,11 @@ csvfile1='../csvfiles/gaiadr3_desidr1_star1.csv'
 csvfile1='../csvfiles/gaiadr3_sdssdr8_star.csv'
 csvfile1='../csvfiles/gaiadr3_sdssdr17_star.csv'
 csvfile1='../csvfiles/gaiadr3_desidr1_star.csv'
+csvfile1='../csvfiles/gaiadr3_desidr2_star.csv'
 #plot_parallax_SNR_histogram(csvfile1)
 sdssdr='DESI DR1'
+sdssdr='DESI DR2'
 plot_cumulativeSNR(csvfile1,sdssdr)
-sys.exit(1)
 
 #SDSS DR17
 csvfile2='../csvfiles/gaiadr3_sdssdr17_star.csv'
@@ -238,6 +239,7 @@ for snr in [5,10,20,50,100,200,400,500]:
    sdssdr='SDSS DR8'
    sdssdr='SDSS DR17'
    sdssdr='DESI DR1'
+   sdssdr='DESI DR2'
    flag_binary=True  ; flag_variable=True
    plot_HRdiagramSNR(csvfile1,snr,sdssdr,flag_binary,flag_variable)
    flag_binary=True  ; flag_variable=False
